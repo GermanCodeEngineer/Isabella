9e5eeef8b4e463050f5fcd00a43f7d4c352f2283
GermanCodeEngineer <GermanCodeEngineer@proton.me>
2025-12-22 14:25:27 +0100

    continued

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from copy import deepcopy
+from copy import copy, deepcopy
 from colorama import init as init_colorama, Fore
 from pmp_manip.utility import grepr_dataclass
 from typing import ClassVar
@@ -16,6 +16,11 @@ class GoodType(SaveInstances):
     @property
     def formatted_name(self):
         return f"{self.text_color}{self.name}{Fore.RESET}"
+
+@grepr_dataclass(grepr_fields=["buy", "sell"])
+class GoodOrderInfo:
+    buy: float
+    sell: float
 
 @grepr_dataclass(grepr_fields=["name", "inputs", "outputs"], unsafe_hash=True, frozen=True)
 class BuildingType(SaveInstances):
@@ -35,8 +40,11 @@ class BuildingType(SaveInstances):
 @grepr_dataclass(grepr_fields=["type", "level", "activation"])
 class Building:
     type: BuildingType
-    level: int
+    level: int # positive
     activation: float = 0 # 0 - 1
+    
+    def lrepr(self) -> str:
+        return f"{self.type.name}(a={self.activation})"
     
     def get_revenue(self, market: MarketFrame, activation_offset: float = 0) -> float:
         base = sum((quantity * market.get_good_price(good)) for good, quantity in self.type.outputs.items())
@@ -53,34 +61,62 @@ class Building:
         return self.get_revenue(market, activation_offset) - self.get_expenses(market, activation_offset)
     
     def next_frame(self, market: MarketFrame) -> None:
-        SMALL_OFFSET = 0.0001
-        profit = self.get_profit(market, activation_offset=SMALL_OFFSET) # even activation=0 should not result in 0
-        more_active_profit = self.get_profit(market, activation_offset=0.1)
-        less_active_profit = self.get_profit(market, activation_offset=-0.1)
-        max_profit, index = three_way_max(profit, more_active_profit, less_active_profit)
-        if (self.activation == 0) and (index == 3):
-            index = 1
-        elif (self.activation > 0) and (max_profit <= 0):
-            index = 3
-        elif (self.activation == 1) and (index == 2):
-            index = 1
-        if index == 1:
-            print("Building", self.type.formatted_name, "kept activation", self.activation)
-        elif index == 2:
-            self.activation = round(min(self.activation + 0.1, 1), 3)
-            print("Building", self.type.formatted_name, f"{Fore.GREEN}increased activation to", self.activation, Fore.RESET)
-        elif index == 3:
+        SMALL_OFFSET = 0#0.0001
+        own_index = market.buildings.index(self)
+        
+        sa_frame = copy(market)
+        sa_frame.prices = sa_frame.update_prices(verbose=False)
+        sa_profit = self.get_profit(sa_frame, activation_offset=SMALL_OFFSET) # even activation=0 should not result in 0
+        
+        if self.activation == 1:
+            ma_profit = sa_profit
+        else:
+            ma_frame = copy(market)
+            ma_frame.buildings = copy(market.buildings)
+            ma_building = copy(market.buildings[own_index])
+            ma_building.activation += 0.1
+            ma_frame.buildings[own_index] = ma_building
+            print("more buildings [", ", ".join([b.lrepr() for b in ma_frame.buildings]), "]")
+            ma_frame.prices = ma_frame.update_prices(indent="more   * ")
+            ma_profit = ma_building.get_profit(ma_frame, activation_offset=SMALL_OFFSET)
+        
+        if self.activation == 0:
+            la_profit = sa_profit
+        else:
+            la_frame = copy(market)
+            la_frame.buildings = copy(market.buildings)
+            la_building = copy(market.buildings[own_index])
+            la_building.activation -= 0.1
+            la_frame.buildings[own_index] = la_building
+            print("less buildings [", ", ".join([b.lrepr() for b in la_frame.buildings]), "]")
+            la_frame.prices = la_frame.update_prices(indent="less * ")
+            la_profit = la_building.get_profit(la_frame, activation_offset=SMALL_OFFSET)
+        
+        # TODO: possibly re-add rule: profit < 0 must make building lower activation
+        max_profit, index = three_way_max(la_profit, sa_profit, ma_profit)
+        if   (index == 1) and ((self.activation == 0) or isinstance(self, FixedBuilding)):
+            index = 2
+        elif (index == 3) and ((self.activation == 1) or isinstance(self, FixedBuilding)):
+            index = 2
+        
+        if   index == 1:
             self.activation = round(max(self.activation - 0.1, 0), 3)
             print("Building", self.type.formatted_name, f"{Fore.RED}lowered activation to", self.activation, Fore.RESET)
-        print("    Rev", round(self.get_revenue(market, SMALL_OFFSET), 3), "Exp", round(self.get_expenses(market, SMALL_OFFSET), 3), 
-            "| profit", round(profit, 3), "->", round(self.get_profit(market, 0), 3))
+        elif index == 2:
+            print("Building", self.type.formatted_name, "kept activation", self.activation)
+        elif index == 3:
+            self.activation = round(min(self.activation + 0.1, 1), 3)
+            print("Building", self.type.formatted_name, f"{Fore.GREEN}increased activation to", self.activation, Fore.RESET)
+        print("    Rev", round(self.get_revenue(market, SMALL_OFFSET), 3), "Exp", round(self.get_expenses(market, SMALL_OFFSET), 3),
+            "| profit", round(sa_profit, 3), "->", round(self.get_profit(market, 0), 3))
+        print(f"    Profit less {Fore.BLUE}{round(la_profit, 3)}{Fore.RESET} more {Fore.YELLOW}{round(ma_profit, 3)}{Fore.RESET}")
     
 @grepr_dataclass(grepr_fields=[])
 class FixedBuilding(Building):
     activation: float = 1
             
-    def get_profit(self, market: MarketFrame, activation_offset: float = 0) -> float:
-        return 1 # Constant Profitability, should not change activation
+    #def get_profit(self, market: MarketFrame, activation_offset: float = 0) -> float:
+    #    return 1 # Constant Profitability, should not change activation
 
 @grepr_dataclass(grepr_fields=["orders", "prices", "buildings"])
 class MarketFrame:
@@ -91,51 +127,57 @@ class MarketFrame:
         return self.prices[good]
     
     @property
-    def orders(self):
-        buy_orders, sell_orders = self.get_good_buy_sell_orders()
-        return {good: {"buy": buy_orders[good], "sell": sell_orders[good]} for good in GoodType.instances}
+    def orders(self) -> dict[GoodType, GoodOrderInfo]:
+        return self.get_good_buy_sell_orders()
    
-    def get_good_buy_sell_orders(self) -> tuple[dict[GoodType, float], dict[GoodType, float]]:
-        buy_orders = dict.fromkeys(GoodType.instances, 0)
-        sell_orders = dict.fromkeys(GoodType.instances, 0)
+    def get_good_buy_sell_orders(self) -> dict[GoodType, GoodOrderInfo]:
+        buy_sell_orders = {good: GoodOrderInfo(buy=0, sell=0) for good in GoodType.instances}
         for building in self.buildings:
             for good, orders in building.type.inputs.items():
-                buy_orders[good] += round(orders * building.activation, 3)
+                buy_sell_orders[good].buy += round(orders * building.activation, 3)
             for good, orders in building.type.outputs.items():
-                sell_orders[good] += round(orders * building.activation, 3)
-        return (buy_orders, sell_orders)
+                buy_sell_orders[good].sell += round(orders * building.activation, 3)
+        return buy_sell_orders
     
-    def next_frame(self) -> MarketFrame:
-        buy_orders, sell_orders = self.get_good_buy_sell_orders()
+    def update_prices(self, verbose: bool=True, indent: str="") -> dict[GoodType, float]:
+        orders = self.get_good_buy_sell_orders()
         new_prices = {}
         for good in GoodType.instances:
-            good_buy_orders = buy_orders[good]
-            good_sell_orders = sell_orders[good]
-
+            good_buy_orders = orders[good].buy
+            good_sell_orders = orders[good].sell
             price = self.get_good_price(good)
-            if good_buy_orders == good_sell_orders:
-                new_price = price
-            elif good_buy_orders < good_sell_orders:
-                ratio = good_buy_orders / good_sell_orders
-                new_price = price * ((ratio - 1) / 3 + 1)
-            else:
-                if good_sell_orders == 0:
-                    new_price = price + 0.1
-                else:
-                    ratio = good_buy_orders / good_sell_orders
-                    new_price = price * ((ratio - 1) / 3 + 1)
             
-            new_price = round(new_price, 3)
-            print("Price of", good.formatted_name, end=" ")
-            if new_price > price:
-                print(f"{Fore.GREEN}increased from", price, "to", new_price, Fore.RESET)
-            elif new_price == price:
-                print("stayed at", price)
+            try:
+                ratio = good_buy_orders / good_sell_orders
+            except ZeroDivisionError:
+                # TODO: improve
+                if good_buy_orders == 0:
+                    new_price = price
+                    #if verbose: print(f"{indent}same from no buys or sells")
+                else:
+                    new_price = max(price, 0.1) * 1.2
+                    #if verbose: print(f"{indent}hiked but no sells")
             else:
-                print(f"{Fore.RED}reduced from", price, "to", new_price, Fore.RESET)
-            print("    Buy", good_buy_orders, "Sell", good_sell_orders)
+                if ratio > 1:
+                    price = max(price, 0.1)
+                new_price = price * ((ratio - 1) / 3 + 1)
+                #if verbose: print(f"{indent}normal adapted", ratio, ratio - 1, (ratio-1)/3+1, "!", price, new_price)
+            new_price = round(new_price, 3)
             new_prices[good] = new_price
-        
+            
+            if verbose:
+                print(f"{indent}Price of", good.formatted_name, end=" ")
+                if new_price > price:
+                    print(f"{Fore.GREEN}increased from", price, "to", new_price, Fore.RESET)
+                elif new_price == price:
+                    print("stayed at", price)
+                else:
+                    print(f"{Fore.RED}reduced from", price, "to", new_price, Fore.RESET)
+                print(f"{indent}    Buy", good_buy_orders, "Sell", good_sell_orders)
+        return new_prices
+    
+    def next_frame(self) -> MarketFrame:
+        new_prices = self.update_prices(verbose=True)        
         print(20*"-")
         new_buildings = deepcopy(self.buildings)
         for building in new_buildings:
